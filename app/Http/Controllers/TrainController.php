663ebf9abd11e2409b0fd138fdeb8fb3ffbfac9d
Giuseppe Indomenico <pippo.son@hotmail.com>
2023-05-25 11:24:39 +0200

    update at current date

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -4,14 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Models\Train;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+
 
 class TrainController extends Controller
 {
 
     public function index()
     {
-        $trains = Train::all();
+        $today = now()->format('Y-m-d');
+        $trains = Train::where('giorno_partenza', '>=', $today)->get();
         return view('home', compact('trains'));
     }
+
 
 }
--- database/seeders/TrainTableSeeder.php
+++ database/seeders/TrainTableSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\Train;
 use Faker\Generator as Faker;
+use Carbon\Carbon;
 
 class TrainTableSeeder extends Seeder
 {
@@ -15,11 +16,13 @@ class TrainTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
+        $currentYear = Carbon::now()->year;
+
         for ($i = 0; $i < 10; $i++) {
             $newTrain = new Train();
             $newTrain->stazione_di_partenza = $faker->city;
             $newTrain->Stazione_di_arrivo = $faker->city;
-            $newTrain->giorno_partenza = $faker->date;
+            $newTrain->giorno_partenza = $faker->dateTimeBetween("$currentYear-01-01", '+1 year')->format('Y-m-d');
             $newTrain->orario_di_partenza = $faker->time;
             $newTrain->orario_di_arrivo = $faker->time;
             $newTrain->codice_treno = $faker->unique()->numberBetween(1000, 9999);
